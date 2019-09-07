8a3df292c8d2058e6ff5ef380764545c35763293
Sam Deakin <rsamdeakin@gmail.com>
2019-09-07 18:31:44 -0400

    Adds diffuse lighting component

--- assets/cubeFS.glsl
+++ assets/cubeFS.glsl
@@ -11,17 +11,22 @@ in float faceid;
 layout(location = 0) out vec4 fragcolour;
 layout(location = 1) out vec4 id;
 
+// Our sun doesn't have any colour bias.
+// Let the user colour dominate the shading
 vec3 sun = normalize(vec3(0.5, 0.5, 0.5));
 
 void main() {
-    sun = normalize(sun);
-
-    float eye_dot_normal = dot(object_eye.xyz, object_normal);
-
     // Set the ambient colour to be brighter for more perpendicular surfaces
     // (With a min of half brightness for a parallel surface)
+    // Our phong shading is modified this way because I dislike the look of 
+    // simple flat ambient lighting.
+    float eye_dot_normal = dot(object_eye, object_normal);
     vec3 ambient = vertex_colour * (eye_dot_normal * 0.5 + 0.5);
 
-    fragcolour = vec4(ambient, 1.0);
+    float normal_dot_sun = dot(object_normal, sun);
+    vec3 diffuse = vertex_colour * normal_dot_sun;
+    diffuse = max(diffuse, 0.0); // Diffuse lighting cannot "subtract" from the total
+
+    fragcolour = vec4(ambient * 0.5 + diffuse * 0.5, 1.0);
     id = vec4(cubeid, faceid);
 }
