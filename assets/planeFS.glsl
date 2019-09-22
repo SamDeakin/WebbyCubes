11efc8a935774cf7819feab4d38a7a9d0dde1557
Sam Deakin <rsamdeakin@gmail.com>
2019-09-22 16:34:19 -0400

    Fades out grid as it gets further away. This doesn't seem to be a good direction to keep heading in

--- assets/planeFS.glsl
+++ assets/planeFS.glsl
@@ -20,17 +20,16 @@ in vec3 plane_point;
 in vec3 plane_normal;
 in vec3 line_point2;
 in vec4 camera_point;
+float grid_fade_min = 0.001;
+float grid_fade_speed = 50.0;
+float grid_fade_value = 0.3;
 
 in vec3 test;
 in vec3 test2;
 
-vec2 calc_second_point() {
-    vec4 camera_point2 = camera_point;
-    float pixelWidth = 2.0 / (u_viewport_size.x);
-    camera_point2.x += pixelWidth;
-
+vec2 get_plane_point(vec4 point) {
     // Project back, and take the 3D coords to get the algebraic eqn of a line
-    vec4 line_point1_4D = u_view_inverse * u_perspective_inverse * camera_point2;
+    vec4 line_point1_4D = u_view_inverse * u_perspective_inverse * point;
     vec3 line_point1 = line_point1_4D.xyz / line_point1_4D.w;
     vec3 line_dir = line_point1 - line_point2;
 
@@ -39,30 +38,35 @@ vec2 calc_second_point() {
     vec3 intersection = d * line_dir + line_point1;
 
     // Pass intersecting point
-    return = intersection.xz;
+    return intersection.xz;
 }
 
-void main() {
-    vec2 distance = fract(world_position - 0.5);
+float on_grid(vec2 position) {
+    vec2 distance = fract(position - 0.5);
     vec2 low_distance = sign(max(distance_threshold - distance, 0.0));
     vec2 high_distance = sign(max(distance - (1.0 - distance_threshold), 0.0));
 
     float total_distance = low_distance.x + low_distance.y + high_distance.x + high_distance.y;
-    fragcolour = vec4(1.0) * sign(total_distance);
+    return sign(total_distance);
+}
+
+void main() {
+    float grid_intensity = on_grid(world_position);
 
     // Nudge the camera one pixel over and see how far away on the plane that is
-    vec2 world_position_2 = calc_second_point();
+    vec2 pixel = 2.0 / u_viewport_size;
+    vec4 point2 = camera_point;
+    point2.xy += pixel;
+    vec2 world_position_2 = get_plane_point(point2);
+    vec4 point3 = camera_point;
+    point3.xy -= pixel;
+    vec2 world_position_3 = get_plane_point(point3);
+    float point_distance = distance(world_position_2, world_position_3);
 
-    // Determine how much of pixel is within line
-    // Alpha blend
-    fragcolour.x = world_position.y / 100.0;
-    fragcolour.y = world_position_2.y / 100.0;
-    fragcolour.z = length(world_position - world_position_2) * 100.0;
-    // fragcolour.xy = abs(world_position - world_position_2);
-    fragcolour.xy = world_position_2;
-    fragcolour.w = 1.0;
-
-    // TODO Get second intersection point and compare to world_position
+    // Obscure and tend towards a flat alpha value as point_distance grows
+    float lerp = clamp(point_distance * grid_fade_speed - grid_fade_min, 0.0, 1.0);
+    float intensity = mix(grid_intensity, grid_fade_value, lerp);
+    fragcolour = vec4(vec3(1.0), 1.0 * intensity);
 
     // We encode the id for the current position a bit weirdly
     // x and y are the absolute world position
