08725729de3a59b145a444ca8b67815b576d8c1a
Sam Deakin <rsamdeakin@gmail.com>
2019-09-22 16:47:26 -0400

    Makes it slightly better by modifying the threshold test for the grid. Needs a fade to opacity for the expanded points

--- assets/planeFS.glsl
+++ assets/planeFS.glsl
@@ -20,9 +20,11 @@ in vec3 plane_point;
 in vec3 plane_normal;
 in vec3 line_point2;
 in vec4 camera_point;
-float grid_fade_min = 0.001;
-float grid_fade_speed = 50.0;
-float grid_fade_value = 0.3;
+float grid_expand_min = 0.1;
+float grid_expand_speed = 10.0;
+float grid_fade_min = 0.5;
+float grid_fade_speed = 30.0;
+float grid_fade_value = 0.5;
 
 in vec3 test;
 in vec3 test2;
@@ -41,18 +43,25 @@ vec2 get_plane_point(vec4 point) {
     return intersection.xz;
 }
 
-float on_grid(vec2 position) {
+// float on_grid(vec2 position) {
+//     vec2 distance = fract(position - 0.5);
+//     vec2 low_distance = sign(max(distance_threshold - distance, 0.0));
+//     vec2 high_distance = sign(max(distance - (1.0 - distance_threshold), 0.0));
+
+//     float total_distance = low_distance.x + low_distance.y + high_distance.x + high_distance.y;
+//     return sign(total_distance);
+// }
+
+float on_grid(vec2 position, float threshold) {
     vec2 distance = fract(position - 0.5);
-    vec2 low_distance = sign(max(distance_threshold - distance, 0.0));
-    vec2 high_distance = sign(max(distance - (1.0 - distance_threshold), 0.0));
+    vec2 low_distance = sign(max(threshold - distance, 0.0));
+    vec2 high_distance = sign(max(distance - (1.0 - threshold), 0.0));
 
     float total_distance = low_distance.x + low_distance.y + high_distance.x + high_distance.y;
     return sign(total_distance);
 }
 
 void main() {
-    float grid_intensity = on_grid(world_position);
-
     // Nudge the camera one pixel over and see how far away on the plane that is
     vec2 pixel = 2.0 / u_viewport_size;
     vec4 point2 = camera_point;
@@ -62,6 +71,10 @@ void main() {
     point3.xy -= pixel;
     vec2 world_position_3 = get_plane_point(point3);
     float point_distance = distance(world_position_2, world_position_3);
+
+    float threshold_mod = max(point_distance * grid_expand_speed - grid_expand_min, 0.0);
+    float threshold = distance_threshold + threshold_mod;
+    float grid_intensity = on_grid(world_position, threshold);
 
     // Obscure and tend towards a flat alpha value as point_distance grows
     float lerp = clamp(point_distance * grid_fade_speed - grid_fade_min, 0.0, 1.0);
