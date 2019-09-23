0e3b3b2347970ed7983753b99cdc19208d70e20d
Sam Deakin <rsamdeakin@gmail.com>
2019-09-23 19:25:45 -0400

    Gets rid of complicated extra-sampling algorithm to prepare for a gradient based algo

--- assets/planeFS.glsl
+++ assets/planeFS.glsl
@@ -15,85 +15,19 @@ in vec3 object_eye;
 vec3 object_normal = vec3(0.0, 1.0, 0.0);
 float distance_threshold = 0.025;
 
-// Extra data used to "supersample" the grid without actually supersampling
-in vec3 plane_point;
-in vec3 plane_normal;
-in vec3 line_point2;
-in vec4 camera_point;
-// Tuning parameters to make the fade out look good
-float grid_expand_speed = 100.0;
-float grid_expand_min = 15.0;
-float grid_depth_speed = 0.02;
-float grid_depth_min = 0.0000001;
-float grid_fade_speed = 30.0;
-float grid_fade_min = 0.1;
-float grid_fade_value = 0.0;
-
-vec2 get_plane_point(vec4 point) {
-    // Project back, and take the 3D coords to get the algebraic eqn of a line
-    vec4 line_point1_4D = u_view_inverse * u_perspective_inverse * point;
-    vec3 line_point1 = line_point1_4D.xyz / line_point1_4D.w;
-    vec3 line_dir = line_point1 - line_point2;
-
-    // Intersect line with plane
-    float d = dot((plane_point - line_point1), plane_normal) / dot(line_dir, plane_normal);
-    vec3 intersection = d * line_dir + line_point1;
-
-    // Pass intersecting point
-    return intersection.xz;
-}
-
-float grid_distance(vec2 position) {
+float on_grid(vec2 position, float threshold) {
     vec2 distance = fract(position - 0.5);
-    vec2 distance_inverse = 1.0 - distance;
-    return min(min(distance.x, distance.y), min(distance_inverse.x, distance_inverse.y));
+    vec2 low_distance = sign(max(threshold - distance, 0.0));
+    vec2 high_distance = sign(max(distance - (1.0 - threshold), 0.0));
+
+    float total_distance = low_distance.x + low_distance.y + high_distance.x + high_distance.y;
+    return sign(total_distance);
 }
 
 void main() {
-    // Nudge the camera one pixel over and see how far away on the plane that is
-    vec2 pixel = 2.0 / u_viewport_size;
-    // We only nudge in one direction for just two samples
-    vec4 point2 = camera_point;
-    // point2.x += pixel.x;
-    point2.y += pixel.y;
-    vec2 world_position_2 = get_plane_point(point2);
-    vec4 point3 = camera_point;
-    // point3.x -= pixel.x;
-    point3.y -= pixel.y;
-    vec2 world_position_3 = get_plane_point(point3);
-    float point_distance = distance(world_position_2, world_position_3);
-
-    // Closest distance to a side of the grid
-    float min_distance = grid_distance(world_position);
-
-    // Here, a value of 0 means on the grid, and we lerp towards transparent
-    // within fade_delta distance of 0
-    float distance_test = min_distance - distance_threshold;
-    float grid_fade = pow(point_distance * grid_expand_speed, grid_expand_min);
-
-    // We also fade based on the depth
-    float grid_depth = distance(plane_point, line_point2);
-    float depth_lerp = clamp(grid_depth * grid_depth_speed - grid_depth_min, 0.0, 1.0);
-
-    float grid_lerp = clamp((distance_test + grid_fade) / grid_fade, 0.0, 1.0);
-    float grid_intensity = mix(1.0, 0.0, grid_lerp);
-    float depth_intensity = mix(grid_intensity, 0.0, depth_lerp);
-
-    // Obscure and tend towards a flat alpha value as point_distance grows
-    float lerp = clamp(point_distance * grid_fade_speed - grid_fade_min, 0.0, 1.0);
-    float intensity = mix(depth_intensity, grid_fade_value, lerp);
-    fragcolour = vec4(vec3(1.0), 1.0 * depth_intensity);
-
-    if (gl_FragCoord.x < u_viewport_size.x / 2.0) {
-        fragcolour.w = 1.0;
-        fragcolour.x = -grid_intensity;
-        fragcolour.y = depth_lerp;
-        fragcolour.z = grid_lerp;
-        // fragcolour.z = sqrt(dot(plane_point - line_point2, plane_point - line_point2)) / 100.0;
-        // fragcolour.x = world_position_2.y;
-        // fragcolour.y = world_position_3.y;
-        // fragcolour.z = point_distance * 100.0;
-    }
+    float threshold = distance_threshold;
+    float grid_intensity = on_grid(world_position, threshold);
+    fragcolour = vec4(vec3(1.0), 1.0 * grid_intensity);
 
     // We encode the id for the current position a bit weirdly
     // x and y are the absolute world position
--- assets/planeVS.glsl
+++ assets/planeVS.glsl
@@ -10,12 +10,6 @@ uniform mat4 u_view_inverse;
 out vec2 world_position;
 out vec3 object_eye;
 
-// Extra data used to "supersample" the grid without actually supersampling
-out vec3 plane_point;
-out vec3 plane_normal;
-out vec3 line_point2; // 1 is calculated in the FS
-out vec4 camera_point;
-
 void main() {
     vec4 view_pos = u_view * vec4(a_position, 1.0);
     gl_Position = u_perspective * view_pos;
@@ -23,15 +17,4 @@ void main() {
 
     vec4 view_eye_dir = vec4(view_pos.xyz * -1.0 / view_pos.w, 0.0);
     object_eye = normalize((u_view_inverse * view_eye_dir).xyz);
-
-    // An untransformed position
-    camera_point = gl_Position;
-
-    vec4 view_eye_point = vec4(vec3(0.0), 1.0);
-    vec4 object_eye_point = u_view_inverse * u_perspective_inverse * view_eye_point;
-    line_point2 = object_eye_point.xyz / object_eye_point.w;
-
-    // Algebraic equation of a plane
-    plane_point = a_position;
-    plane_normal = vec3(0.0, 1.0, 0.0);
 }
