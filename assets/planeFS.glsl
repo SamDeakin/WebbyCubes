cb7c49b56a4b01ab819edb1f578fc77e0cbc0fc2
Sam Deakin <rsamdeakin@gmail.com>
2019-10-13 01:31:44 -0400

    Fixes bug where the grid would have weird interference patterns at some angles only

--- assets/planeFS.glsl
+++ assets/planeFS.glsl
@@ -17,7 +17,7 @@ in vec3 perspective_eye;
 in vec3 perspective_normal;
 
 float distance_threshold = 0.025;
-float gradient_fadeout_threshold = 0.4;
+float gradient_fadeout_threshold = 0.45;
 float PI_BY_2 = 1.57079632679489661;
 
 // Returns 1.0 when the position is within threshold of the grid, 0.0 otherwise.
@@ -26,6 +26,18 @@ float on_grid(vec2 position, float threshold, vec2 dx, vec2 dy) {
     position = fract(position - 0.5);
 
     vec2 maxdist = vec2(0.0);
+    maxdist = max(maxdist, ceil(position + dx + threshold) - ceil(position));
+    maxdist = max(maxdist, ceil(position + dy + threshold) - ceil(position));
+    maxdist = max(maxdist, floor(position) - floor(position - dx - threshold));
+    maxdist = max(maxdist, floor(position) - floor(position - dy - threshold));
+
+    // We have to ensure we test ceil and floor in the correct dx,dy directions
+    // for example: in the case where the user rotates 180 degrees.
+    // We could do some calculation on dx,dy values to avoid all 8 tests, but
+    // it would probably be as much logic as just flipping the values and doing
+    // twice as many tests.
+    dx = -dx;
+    dy = -dy;
     maxdist = max(maxdist, ceil(position + dx + threshold) - ceil(position));
     maxdist = max(maxdist, ceil(position + dy + threshold) - ceil(position));
     maxdist = max(maxdist, floor(position) - floor(position - dx - threshold));
